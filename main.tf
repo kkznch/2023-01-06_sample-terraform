aa60c2ae104a058ac6261472b2b320f89f600eb2
kkznch <kkznch710md@gmail.com>
2023-01-07 15:38:58 +0900

    EC2起動するまで書いた

---
+++ main.tf
@@ -0,0 +1,69 @@
+# Terraformプロバイダの設定
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.49.0"
+    }
+  }
+}
+
+# Networkの設定
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "this" {
+  vpc_id                  = aws_vpc.this.id
+
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "10.0.128.0/24"
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "this" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+  route_table_id         = aws_route_table.this.id
+}
+
+resource "aws_route_table_association" "this" {
+  route_table_id = aws_route_table.this.id
+  subnet_id      = aws_subnet.this.id
+}
+
+# EC2の設定
+resource "aws_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami = "ami-0bba69335379e17f8"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.this.id
+  private_ip = "10.0.128.100"
+  associate_public_ip_address = true
+  security_groups = [aws_security_group.this.id]
+}
